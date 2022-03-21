27889b27be98350a7b5007d0c344cb8c450bd16d
Ruthvi Reddy <94678422+Ruthvi2809@users.noreply.github.com>
2022-03-21 06:39:07 +0000

    p3 original

---
+++ p5original.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter the numbers:");
+  scanf("%d",&a);
+  return a;
+}
+int_gcd(int a,int b)
+{
+  int gcd=1;
+  for(int i=2;i<a&&i<b;i++)
+    {
+      if(a%i==0&&i==0)
+      {
+        gcd=i;
+      }
+
+      
+    }
+  return gcd;
+}
+void out_put(int a,int b,int gcd)
+{
+  printf("GCD of %d and %d is %d "a,b,gcd);
+}
+int main()
+{
+  int a,b,gcd;
+  a=input();
+  b=input();
+  gcd=_gcd(a,b);
+  ot_put(a,b,gcd);
+  return 0;
+}
